404347d6c68b4f371270c311bf4e7108a3a0f6a3
fredrickb <fredrickb@netcompany.com>
2017-12-04 19:13:26 +0100

    Added AppBar on root page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const NetCup = () => (
+    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <App title="NetCup" />
+    </MuiThemeProvider>
+);
+
+ReactDOM.render(<NetCup />, document.getElementById('root'));
 registerServiceWorker();
