0b31704de5bfedd3acb4138f7330790fc3060d2e
fredrickb <fredrickb@netcompany.com>
2018-05-15 19:00:59 +0200

    Leaderboard is now sorted by points

---
+++ src/modules/leaderboard/utilities/sort-participants.js
@@ -0,0 +1,13 @@
+const sortParticipants = participants => {
+  return participants.sort((e1, e2) => {
+    if (e1.points > e2.points) {
+      return -1;
+    }
+    if (e2.points > e1.points) {
+      return 1;
+    }
+    return 0;
+  });
+};
+
+export default sortParticipants;
