443d186d28a2756389f062075325b6ec717dd79c
Fredrick Biering <bieringf@gmail.com>
2019-02-10 13:55:30 +0100

    replaced circular spinner with linear-loader (#8)

--- src/modules/events/components/EventList.jsx
+++ src/modules/events/components/EventList.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'react-flexbox-grid/lib';
+import { Col } from 'react-flexbox-grid/lib';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import Content from '../../common/components/Content';
+import { LinearProgress } from '@material-ui/core';
 
 const styles = {
   wrapper: {
@@ -28,33 +28,33 @@ class EventList extends Component {
   render() {
     const { classes, events, loading } = this.props;
     return (
-      <Content className={classes.wrapper}>
+      <div>
         {loading &&
-          <Row center="xs">
-            <CircularProgress size={60} />
-          </Row>
+          <LinearProgress />
         }
-        {events && events.length ?
-          events.map(event =>
-            <Col md={12}>
-              <Card className={classes.card}>
-                <CardMedia
-                  className={classes.media}
-                  image={event.image}
-                  title={event.title}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {event.title}
-            </Typography>
-                  <Typography component="p">{event.description}
-            </Typography>
-                </CardContent>
-              </Card>
-            </Col>
-          ) : null
-        }
-      </Content>
+        <Content className={classes.wrapper}>
+          {events && events.length ?
+            events.map(event =>
+              <Col md={12}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.media}
+                    image={event.image}
+                    title={event.title}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {event.title}
+                    </Typography>
+                    <Typography component="p">{event.description}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Col>
+            ) : null
+          }
+        </Content>
+      </div>
     );
   }
 }
