bf3092bbaccdd11099e39ea0765c944e0329c8c5
Fredrick <bieringf@gmail.com>
2019-05-26 19:53:48 +0200

    PrivateRoute now has an extra option for admin-only routes

--- src/modules/auth/containers/PrivateRoute.js
+++ src/modules/auth/containers/PrivateRoute.js
@@ -2,7 +2,10 @@ import { connect } from 'react-redux';
 import PrivateRoute from '../components/PrivateRoute';
 
 export default connect(
-  state => ({
-    authenticated: state.auth.loggedIn,
+  (state, ownProps) => ({
+    authenticated: ownProps.adminOnly ?
+      state.auth.loggedIn && state.auth.currentUser.isAdmin 
+      : 
+      state.auth.loggedIn,
   }),
 )(PrivateRoute);
