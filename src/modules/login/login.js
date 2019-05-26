02508ab3540adceccaf9901a048ef70f13303d9e
Fredrick <bieringf@gmail.com>
2019-05-26 12:19:49 +0200

    Removed unnecessary duplicate update of user since auth-listener alread handles this

--- src/modules/login/login.js
+++ src/modules/login/login.js
@@ -1,12 +1,10 @@
 import { login as firebaseLogin } from './services/auth-service';
 import { APP } from '../../constants';
-import { updateCurrentUser } from '../auth/auth';
 
 const START_LOGIN = `${APP}/login/startLogin`;
-const LOGIN_SUCCESS = `${APP}/login/loginSuccess`;
 const LOGIN_FAILURE = `${APP}/login/loginFailure`;
 const LOGIN_FINISH = `${APP}/login/finishedLogin`;
-const DISMISS_ERROR_MESSAGE = `${APP}/login/dismiss_error_message`;
+const DISMISS_ERROR_MESSAGE = `${APP}/login/dismissErrorMessage`;
 
 const defaultState = {
   loading: false,
@@ -18,9 +16,6 @@ export default function reducer(state = defaultState, action) {
   switch (action.type) {
     case START_LOGIN: {
       return { ...state, loading: true, showErrorMessage: false, errorMessage: '' };
-    }
-    case LOGIN_SUCCESS: {
-      return state;
     }
     case LOGIN_FAILURE: {
       return { ...state, showErrorMessage: true, errorMessage: action.payload.message };
@@ -36,20 +31,14 @@ export default function reducer(state = defaultState, action) {
 }
 
 const startLogin = { type: START_LOGIN };
-const loginSuccess = { type: LOGIN_SUCCESS };
 const loginFailure = ({ code, message }) => ({ type: LOGIN_FAILURE, payload: { code, message } });
 const finishedLogin = { type: LOGIN_FINISH };
 export const dismissErrorMessage = { type: DISMISS_ERROR_MESSAGE };
 
 export const login = (userEmail, password, redirect) => async dispatch => {
   dispatch(startLogin);
-
   try {
-    const res = await firebaseLogin(userEmail, password);
-    const { uid, email, displayName, emailVerified } = res.user;
-
-    dispatch(updateCurrentUser({ uid, email, displayName, emailVerified }));
-    dispatch(loginSuccess);
+    await firebaseLogin(userEmail, password);
     redirect();
   } catch (e) {
     dispatch(loginFailure(e));
